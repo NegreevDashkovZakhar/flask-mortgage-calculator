6c0b1f9c28aae5699d141294b005b85cc8a57883
Negreev-Dashkov Zakhar <negreevdashkov.zakhar@gmail.com>
2023-04-22 22:15:43 +0600

    [ ]Corrected post/get differentiation logic

--- src/main.py
+++ src/main.py
@@ -7,15 +7,15 @@ app = Flask("Mortgage calculator")
 
 @app.route('/', methods=['post', 'get'])
 def annuity_calculator():
-    result = 0
     if request.method == 'POST':
         amount = int(request.form.get('mortgage_amount'))
         first_pay = int(request.form.get('first_pay'))
         duration = int(request.form.get('mortgage_duration'))
         interest_rate = float(request.form.get('interest_rate'))
         result = get_annuity_mortgage(amount, first_pay, duration, interest_rate)
-
-    return render_template('annuity.html', result=round(result, 2))
+        return render_template('annuity.html', result=round(result, 2))
+    else:
+        return render_template('annuity.html')
 
 
 if __name__ == '__main__':
